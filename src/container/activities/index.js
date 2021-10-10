4f72330cbf4aecb96bd41bb40591f0f76bc0aa3b
Hip <cnhh85@gmail.com>
2021-10-11 03:34:32 +0700

    add picture to project, add title, decript and button to head, add decoration circles to the head

--- src/container/activities/index.js
+++ src/container/activities/index.js
@@ -1,18 +1,62 @@
 import React from 'react';
 
+import Pic1 from '../../assets/img/activities/pic1.png';
+import Pic2 from '../../assets/img/activities/pic2.png';
+import Pic3 from '../../assets/img/activities/pic3.png';
 // import NavBar from '../../component/navbar';
 // import Footer from '../../component/footer';
-import { sectionWrapper, topContainer, midContainer, botContainer } from './style';
+import {
+    SectionWrapper,
+    TopContainer,
+    MidContainer,
+    BottomContainer,
+    PageTitle,
+    TitleInfo,
+    TitleHead,
+    TitleSpan,
+    Detail,
+    RegisButton,
+    CirclesContainer,
+    EllipseImg1,
+    EllipseImg2,
+    EllipseImg3,
+    BigCircle,
+    SmallCircle,
+    TrainC,
+} from './style';
 
 const Activities = () => {
     return (
-        <sectionWrapper>
+        <SectionWrapper>
             {/* <NavBar /> */}
-            <topContainer></topContainer>
-            <topContainer></topContainer>
-            <midContainer></midContainer>
+            <TopContainer>
+                <PageTitle>
+                    <TitleInfo>
+                        <TitleHead>
+                            MỘT SỐ <br /> HOẠT ĐỘNG CỦA CLB
+                            <br />
+                            <TitleSpan>FCODE</TitleSpan>
+                        </TitleHead>
+                        <Detail>
+                            Đến với FCode các bạn sẽ được tham gia rất nhiều hoạt động bổ ích, cũng
+                            như trau dồi thêm được nhiều kinh nghiệp quý giá
+                        </Detail>
+                        <RegisButton>ĐĂNG KÍ</RegisButton>
+                    </TitleInfo>
+                    <CirclesContainer>
+                        <BigCircle></BigCircle>
+                        <EllipseImg1 src={Pic1}></EllipseImg1>
+                        <EllipseImg2 src={Pic2}></EllipseImg2>
+                        <SmallCircle></SmallCircle>
+                        <EllipseImg3 src={Pic3}></EllipseImg3>
+                    </CirclesContainer>
+                </PageTitle>
+                <TrainC></TrainC>
+            </TopContainer>
+            <MidContainer></MidContainer>
+            <BottomContainer></BottomContainer>
             {/* <Footer /> */}
-        </sectionWrapper>
+        </SectionWrapper>
     );
 };
 
--- src/container/activities/style.js
+++ src/container/activities/style.js
@@ -1,9 +1,99 @@
 import styled from 'styled-components';
 
-export const sectionWrapper = styled.div``;
+export const SectionWrapper = styled.div``;
 
-export const topContainer = styled.div``;
+export const TopContainer = styled.div`
+    margin: 0;
+    width: 100%;
+    background-color: rgba(0, 219, 150, 0.03);
+`;
 
-export const midContainer = styled.div``;
+export const PageTitle = styled.div``;
 
-export const botContainer = styled.div``;
+export const TitleInfo = styled.div`
+    padding-top: 250px;
+    margin-left: 100px;
+    z-index: 1000;
+`;
+
+export const TitleHead = styled.h1`
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 72px;
+    line-height: 84px;
+    color: #262727;
+`;
+
+export const TitleSpan = styled.span`
+    color: #00d17d;
+`;
+
+export const Detail = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 120%;
+    color: rgba(0, 0, 0, 0.6);
+    width: 530px;
+`;
+
+export const RegisButton = styled.button`
+    width: 98px;
+    height: 44px;
+    left: calc(50% - 98px / 2);
+    top: 0px;
+    background: #00db96;
+    border-radius: 4px;
+    border: none;
+    color: white;
+`;
+
+export const CirclesContainer = styled.div`
+    z-index: -1;
+`;
+
+export const EllipseImg1 = styled.img`
+    position: absolute;
+    left: 63%;
+    top: 190px;
+`;
+export const EllipseImg2 = styled.img`
+    position: absolute;
+    left: 68%;
+    top: 326px;
+`;
+export const EllipseImg3 = styled.img`
+    position: absolute;
+    left: 50%;
+    top: 500px;
+`;
+
+export const BigCircle = styled.div`
+    position: absolute;
+    width: 333px;
+    height: 333px;
+    left: 67%;
+    top: 200px;
+    background: #00d17d;
+    border-radius: 100%;
+`;
+
+export const SmallCircle = styled.div`
+    position: absolute;
+    width: 172px;
+    height: 172px};
+    left: 57%;
+    top: 592px;
+    background: #00d17d;
+    border-radius: 100%;
+`;
+
+export const TrainC = styled.div`
+    margin-top: 150px;
+    padding: 100px;
+`;
+export const MidContainer = styled.div``;
+
+export const BottomContainer = styled.div``;
