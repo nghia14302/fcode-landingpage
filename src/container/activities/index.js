47b6b7c4dc33b6884d1ca2fcfdc1d876f5ee2569
Hip <cnhh85@gmail.com>
2021-10-11 00:53:15 +0700

    add page containers

--- src/container/activities/index.js
+++ src/container/activities/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 // import NavBar from '../../component/navbar';
 // import Footer from '../../component/footer';
-import { sectionWrapper } from './style';
+import { sectionWrapper, topContainer, midContainer, botContainer } from './style';
 
-function Activities() {
+const Activities = () => {
     return (
         <sectionWrapper>
             {/* <NavBar /> */}
-
+            <topContainer></topContainer>
+            <topContainer></topContainer>
+            <midContainer></midContainer>
             {/* <Footer /> */}
         </sectionWrapper>
     );
-}
+};
 
 export default Activities;
--- src/container/activities/style.js
+++ src/container/activities/style.js
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
 export const sectionWrapper = styled.div``;
+
+export const topContainer = styled.div``;
+
+export const midContainer = styled.div``;
+
+export const botContainer = styled.div``;
