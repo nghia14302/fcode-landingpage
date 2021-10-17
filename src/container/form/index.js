1a689a5d5b1f7fe6dda0806b94792e90429a9c06
Hip <cnhh85@gmail.com>
2021-10-18 02:33:21 +0700

    add raw style for form

--- src/container/form/index.js
+++ src/container/form/index.js
@@ -1,9 +1,81 @@
 import React from 'react';
 
-import { SectionWrapper } from './style.js';
+import Image from '../../assets/form/bg.png';
+import {
+    SectionWrapper,
+    FormContainer,
+    FormContent,
+    ImageContainer,
+    FormImage,
+    FormTitle,
+    FormHead,
+    FormDetail,
+    FormWrap,
+    Label,
+    NameInput,
+    InputSmall,
+    InputSmallLeft,
+    OptionContainer,
+    Select,
+    Option,
+    SmallSelect,
+    FormLineWrap,
+    Input,
+    CheckBox,
+    SubmitButton,
+} from './style.js';
 
 const Form = () => {
-    return <SectionWrapper></SectionWrapper>;
+    return (
+        <SectionWrapper>
+            <FormContainer>
+                <FormContent>
+                    <FormTitle>
+                        <FormHead>Đăng kí</FormHead>
+                        <FormDetail>
+                            Bạn điền đầy đủ các thông tin dưới đây để nhận thử thách sắp tới từ CLB
+                            nhé!
+                        </FormDetail>
+                    </FormTitle>
+                    <FormWrap>
+                        <Label>Tên</Label>
+                        <NameInput>
+                            <InputSmallLeft placeholder="Nhập tên của bạn"></InputSmallLeft>
+                            <InputSmall placeholder="Nhập họ của bạn"></InputSmall>
+                        </NameInput>
+                        <Label>Ngành học</Label>
+                        <Select>
+                            <Option>Kĩ thuật phần mềm</Option>
+                            <Option></Option>
+                            <Option></Option>
+                        </Select>
+                        <OptionContainer>
+                            <FormLineWrap>
+                                <Label>Mã số sinh viên</Label>
+                                <InputSmallLeft></InputSmallLeft>
+                            </FormLineWrap>
+                            <FormLineWrap>
+                                <Label>Kì học hiện tại</Label>
+                                <SmallSelect>
+                                    <Option></Option>
+                                    <Option></Option>
+                                    <Option></Option>
+                                </SmallSelect>
+                            </FormLineWrap>
+                        </OptionContainer>
+                        <Label>Số điện thoại</Label>
+                        <Input></Input>
+                        <CheckBox type="checkbox" id="confirm" name="confirm"></CheckBox>
+                        <Label htmlFor="confirm">Tôi đồng ý tham gia thử thách của CLB FCode</Label>
+                    </FormWrap>
+                    <SubmitButton>ĐẮNG KÍ</SubmitButton>
+                </FormContent>
+                <ImageContainer>
+                    <FormImage src={Image} draggable="false"></FormImage>
+                </ImageContainer>
+            </FormContainer>
+        </SectionWrapper>
+    );
 };
 
 export default Form;
--- src/container/form/style.js
+++ src/container/form/style.js
@@ -1,3 +1,180 @@
 import styled from 'styled-components';
 
 export const SectionWrapper = styled.div``;
+
+export const FormContainer = styled.div`
+    padding-top: 100px;
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+`;
+
+export const FormContent = styled.div`
+    width: 450px;
+    height: 700px;
+    align-items: center;
+    margin: auto;
+`;
+
+export const FormTitle = styled.div``;
+
+export const FormHead = styled.h2`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 64px;
+    color: #262727;
+`;
+
+export const FormDetail = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    color: rgba(0, 0, 0, 0.6);
+`;
+
+export const FormWrap = styled.form`
+    margin-top: 50px;
+`;
+
+export const Label = styled.label`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 16px;
+    color: rgba(0, 0, 0, 0.6);
+    margin-bottom: 10px;
+`;
+
+export const NameInput = styled.div`
+    margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+    height: 56px;
+    width: 448px;
+    border: 1px solid rgba(0, 0, 0, 0.36);
+    box-sizing: border-box;
+    border-radius: 4px;
+    margin-right: 26px;
+    padding-left: 15px;
+    margin-bottom: 20px;
+    padding-right: 15px;
+    &:focus {
+        outline: none;
+    }
+    ::placeholder {
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+        color: rgba(0, 0, 0, 0.36);
+    }
+`;
+
+export const InputSmallLeft = styled.input`
+    width: 211px;
+    height: 56px;
+    border: 1px solid rgba(0, 0, 0, 0.36);
+    box-sizing: border-box;
+    border-radius: 4px;
+    margin-right: 26px;
+    padding-left: 15px;
+    padding-right: 15px;
+    &:focus {
+        outline: none;
+    }
+    ::placeholder {
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+        color: rgba(0, 0, 0, 0.36);
+    }
+`;
+
+export const InputSmall = styled.input`
+    width: 211px;
+    height: 56px;
+    border: 1px solid rgba(0, 0, 0, 0.36);
+    box-sizing: border-box;
+    border-radius: 4px;
+    padding-left: 15px;
+    padding-right: 15px;
+    &:focus {
+        outline: none;
+    }
+    ::placeholder {
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+        color: rgba(0, 0, 0, 0.36);
+    }
+`;
+
+export const OptionContainer = styled.div`
+    display: flex;
+    margin-bottom: 20px;
+`;
+
+export const Select = styled.select`
+    width: 448px;
+    height: 56px;
+    padding-left: 15px;
+    outline: none;
+    border: 1px solid rgba(0, 0, 0, 0.36);
+    margin-bottom: 20px;
+`;
+
+export const Option = styled.option`
+    height: 100px;
+`;
+
+export const SmallSelect = styled.select`
+    width: 211px;
+    height: 56px;
+    border: 1px solid rgba(0, 0, 0, 0.36);
+`;
+
+export const SmallOption = styled.option``;
+
+export const FormLineWrap = styled.div`
+    width: 50%;
+    margin-right: 13px;
+`;
+
+export const CheckBox = styled.input`
+    margin-right: 12px;
+`;
+
+export const SubmitButton = styled.button`
+    width: 448px;
+    height: 44px;
+    background: #00d17d;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12),
+        0px 2px 4px rgba(0, 0, 0, 0.2);
+    border-radius: 4px;
+    margin-top: 50px;
+    color: white;
+    border: none;
+    cursor: pointer;
+`;
+
+export const ImageContainer = styled.div`
+    margin-top: auto;
+    flex: 0 0 50%;
+    @media (max-width: 1024px) {
+        display: none;
+    }
+`;
+
+export const FormImage = styled.img`
+    width: 100%;
+    height: 100%;
+`;
