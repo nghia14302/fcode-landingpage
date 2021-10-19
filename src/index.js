6a93ce34af4cefc51a857ef1da62f53c789f0dad
nghia14302 <nghia14302@gmail.com>
2021-10-19 12:58:44 +0700

    chang route url

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <MemoryRouter forceRefresh={false}>
+        <BrowserRouter forceRefresh={false}>
             <App />
-        </MemoryRouter>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
